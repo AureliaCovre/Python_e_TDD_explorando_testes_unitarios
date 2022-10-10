1b8e75fe50b42e78e381655c5014d5c6d45ce1a6
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-10-10 11:55:16 -0300

    Create test_bytebank.py

---
+++ tests/test_bytebank.py
@@ -0,0 +1,67 @@
+from codigo.bytebank import Funcionario
+import pytest
+from pytest import mark #Para colocar tags nos testes
+
+# Para rodar o teste digite "pytest -v" no terminal 
+# Para rodar apenas um teste: "pytest -k <palavra q contenha apenas no nome daquele teste"
+# Para rodar apenas um , de forma mais verbosa, add o -v: "pytest -v -k <palavra q contenha apenas no nome daquele teste"
+# Para executar o pytest cov:  pytest --cov=codigo --cov-report term-missing
+
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = '13/03/2000' # Given-Contexto
+        esperado = 22
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.__str__() # When       
+
+        assert resultado == esperado #Then 
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome() # When
+
+        assert resultado == esperado
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000 #Given
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000 
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario() # When
+        resultado = funcionario_teste.salario         
+
+        assert resultado == esperado #Then 
+
+    
+    """ Teste para método CALCULAR_BONUS quando o salario é de mil."""
+    @mark.calcular_bonus #Tag para associar testes
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000 #Given
+        esperado = 100 
+
+        funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
+        resultado = funcionario_teste.calcular_bonus() # When       
+
+        assert resultado == esperado #Then 
+
+    """ Teste para o  Exception do método CALCULAR_BONUS """  
+    @mark.calcular_bonus #Tag para associar testes  
+    def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):
+            entrada = 1000000 #Given
+            esperado = 100 
+
+            funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
+            resultado = funcionario_teste.calcular_bonus() # When       
+
+            assert resultado #Then 
+
+    def test_retorno_str(self):
+        nome, data_nascimento, salario = 'Teste', '12/03/2000',1000 #Given
+        esperado = 'Funcionario(Teste, 12/03/2000, 1000)' 
+
+        funcionario_teste = Funcionario(nome, data_nascimento, salario)
+        resultado = funcionario_teste.__str__() # When       
+
+        assert resultado == esperado #Then 
